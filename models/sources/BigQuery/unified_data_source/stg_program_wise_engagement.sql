62179ea77e776013dae522346ba80f44f43799f6
Swapneel Rane <swapneel@goalkeep.net>
2023-01-11 11:20:26 +0000

    add deduplicated int_program_wise_engagement table

--- models/sources/BigQuery/unified_data_source/stg_program_wise_engagement.sql
+++ models/sources/BigQuery/unified_data_source/stg_program_wise_engagement.sql
@@ -11,6 +11,9 @@ with
             program_end_date,
             user_age
         from {{ source("unified_data_source", "program_wise_engagement") }}
+        where
+            cast(user_created_on as timestamp)
+            <= timestamp_sub(current_timestamp(), interval 100 minute)
     )
 
 select *
