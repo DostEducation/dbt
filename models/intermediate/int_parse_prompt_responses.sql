fe7a3d4dc2c3de1daa47a0a9b3fda4cf63ce2b71
Swapneel Rane <swapneel@goalkeep.net>
2023-01-11 13:06:46 +0000

    get content_id

--- models/intermediate/int_parse_prompt_responses.sql
+++ models/intermediate/int_parse_prompt_responses.sql
@@ -2,6 +2,7 @@ with
     ivr_prompt_response as (select * from {{ ref("stg_ivr_prompt_response") }}),
     all_call_records as (select * from {{ ref("stg_all_calls_records") }}),
     program_sequence as (select * from {{ ref("stg_program_sequence") }}),
+    content_version as (select * from {{ ref("stg_content_version") }}),
 
     -- add deduplicated engagement level
     get_program_sequence_id as (
@@ -11,9 +12,11 @@ with
             all_call_records.content_version_id,
             program_sequence.program_id,
             program_sequence.module_id,
+            content_version.content_id
         from ivr_prompt_response
             left join all_call_records using (unified_call_id, data_source)
             left join program_sequence using (program_sequence_id, data_source)
+            left join content_version using (content_version_id, data_source)
     )
 
 select *
--- models/sources/BigQuery/unified_data_source/stg_content_version.sql
+++ models/sources/BigQuery/unified_data_source/stg_content_version.sql
@@ -1,4 +1,16 @@
-select 
-    *
-from {{source ('unified_data_source','content_version') }}
-where migrated_on <= CURRENT_TIMESTAMP() - INTERVAL 100 MINUTE
+with
+    content_version as (
+        select
+            id as content_version_id,
+            data_source,
+            content_id,
+            language_id,
+            duration as content_duration,
+            status as content_status,
+            version as content_version,
+        from {{ source("unified_data_source", "content_version") }}
+        where migrated_on <= current_timestamp() - interval 100 minute
+    )
+
+select *
+from content_version
