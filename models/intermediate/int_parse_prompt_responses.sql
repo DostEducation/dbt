d69a425e01150ec1fee45ace58dc6f06d4ac283d
Swapneel Rane <swapneel@goalkeep.net>
2023-01-11 19:08:40 +0000

    join with program table

--- models/intermediate/int_parse_prompt_responses.sql
+++ models/intermediate/int_parse_prompt_responses.sql
@@ -4,6 +4,7 @@ with
     program_sequence as (select * from {{ ref("stg_program_sequence") }}),
     content_version as (select * from {{ ref("stg_content_version") }}),
     content as (select * from {{ ref("stg_content") }}),
+    program as (select * from {{ ref("stg_program") }}),
     q_r_p_map as (select * from {{ ref("int_question_response_placement_map") }}),
 
     -- add deduplicated engagement level
@@ -17,10 +18,12 @@ with
             program_sequence.module_id,
             content_version.content_id,
             content.* except (content_id, data_source),
+            program.* except (program_id, data_source),
             q_r_p_map.* except (keypress, data_source, program_sequence_id, content_id, webhook_response_value)
         from ivr_prompt_response
         left join all_call_records using (unified_call_id, data_source)
         left join program_sequence using (program_sequence_id, data_source)
+        left join program using (program_id, data_source)
         left join content_version using (content_version_id, data_source)
         left join content on content.content_id = content_version.content_id and content.data_source = content_version.data_source
         left join
