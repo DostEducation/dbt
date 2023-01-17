d1228348dfc7cf701fa07cefda5c86bf14025b53
Swapneel Rane <swapneel@goalkeep.net>
2023-01-17 07:50:01 +0000

    resolve issue with duplication of prompt responses after parsing

--- models/intermediate/int_parse_prompt_responses.sql
+++ models/intermediate/int_parse_prompt_responses.sql
@@ -19,32 +19,32 @@ with
             program.* except (program_id, data_source),
             content_version.content_id,
             content.* except (content_id, data_source),
-            -- q_r_p_map.* except (keypress, data_source, program_sequence_id, content_id, webhook_response_value)
+            q_r_p_map.* except (keypress, data_source, program_sequence_id, content_id, webhook_response_value)
         from ivr_prompt_response
         left join unified_call_records using (unified_call_id, data_source)
         left join program_sequence using (program_sequence_id, data_source)
         left join program using (program_id, data_source)
         left join content_version using (content_version_id, data_source)
         left join content on content.content_id = content_version.content_id and content.data_source = content_version.data_source
-        -- left join
-        --     q_r_p_map
-        --     on
-        --     q_r_p_map.keypress = ivr_prompt_response.keypress
-        --     and ivr_prompt_response_created_on >= question_placement_activation_date
-        --     and (
-        --         ivr_prompt_response_created_on < question_placement_deactivation_date
-        --         or question_placement_deactivation_date is null
-        --     )
-        --     and (
-        --         (
-        --             q_r_p_map.program_sequence_id = program_sequence.program_sequence_id
-        --             and q_r_p_map.data_source = 'admindashboard'
-        --         )
-        --         or (
-        --             q_r_p_map.content_id = program_sequence.content_id
-        --             and q_r_p_map.data_source = 'rp_ivr'
-        --         )
-        --     )
+        left join
+            q_r_p_map
+            on
+            q_r_p_map.keypress = ivr_prompt_response.keypress
+            and ivr_prompt_response_created_on >= question_placement_activation_date
+            and (
+                ivr_prompt_response_created_on < question_placement_deactivation_date
+                or question_placement_deactivation_date is null
+            )
+            and (
+                (
+                    q_r_p_map.program_sequence_id = program_sequence.program_sequence_id
+                    and q_r_p_map.data_source = 'admindashboard'
+                )
+                or (
+                    q_r_p_map.content_id = program_sequence.content_id
+                    and q_r_p_map.data_source = 'rp_ivr'
+                )
+            )
     )
 
 select *
