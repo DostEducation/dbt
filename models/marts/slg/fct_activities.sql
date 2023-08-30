66b5483c895ec611de982d151838e4ba51132fb3
goalkeep@dosteducation.com <goalkeep@dosteducation.com>
2023-08-30 06:41:32 +0000

    removed int activities and added the code to fct activities and renamed file fct registrations daily

--- models/marts/slg/fct_activities.sql
+++ models/marts/slg/fct_activities.sql
@@ -1,5 +1,33 @@
-with activities as (select * from {{ ref('int_activities') }})
-
-select
-    *
-from activities
+with
+    activities as (select * from {{ ref('stg_activities') }}),
+    geographies as (select * from {{ ref('int_geographies') }}),
+    dost_team as (select * from {{ ref('stg_dost_team') }}),
+    joining_all_geographies as (
+        select
+            activities.*,
+            state_name,
+            district_name,
+            block_name,
+            sector_name,
+            centre_name
+        from
+            activities
+            left join geographies
+                on case
+                    when activities.activity_level = 'State' then activities.state_id = geographies.state_id and geographies.activity_level = 'State'
+                    when activities.activity_level = 'District' then activities.district_id = geographies.district_id and geographies.activity_level = 'District'
+                    when activities.activity_level = 'Block' then activities.block_id = geographies.block_id and geographies.activity_level = 'Block'
+                    when activities.activity_level = 'Sector' then activities.sector_id = geographies.sector_id and geographies.activity_level = 'Sector'
+                    when activities.activity_level = 'Centre' then activities.centre_id = geographies.centre_id and geographies.activity_level = 'Centre'
+                end
+    ),
+    adding_dost_team_info as (
+    select
+        joining_all_geographies.*,
+        dost_member_name,
+        role
+    from joining_all_geographies
+    left join dost_team using (dost_team_id)
+    )
+select *
+from adding_dost_team_info
