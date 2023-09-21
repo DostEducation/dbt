57accad68b8de053d069c72441221c0642d7090a
goalkeep@dosteducation.com <goalkeep@dosteducation.com>
2023-09-21 11:29:46 +0000

    rename the file and remove follow up

--- models/marts/slg/fct_sectorwise_registration.sql
+++ models/marts/slg/fct_sectorwise_registration.sql
@@ -2,25 +2,23 @@ with centres as (select * from {{ ref('int_centre_geographies') }}),
     activities as (select * from {{ ref('fct_activities') }}),
     calculating_beneficiaries_centres as (
         select 
-            district_id,
             district_name,
-            block_id,
             block_name,
             sector_id,
             sector_name,
             sum(coalesce(cast(centres.total_beneficiaries as int),0)) as total_beneficiaries,
         from centres
-        group by 1,2,3,4,5,6
+        group by 1,2,3,4
         order by sum(coalesce(cast(centres.total_beneficiaries as int),0)) desc
     ),
     calculating_total_onboarded as (
         select
             sector_id,
             sector_name,
-            sum(coalesce(cast(centre_onboarding as int),0) + coalesce(cast(home_onboarding as int),0) + coalesce(cast(follow_up as int),0) + coalesce(cast(community_engagement_onboarded as int),0)) as reported_registrations
+            sum(coalesce(cast(centre_onboarding as int),0) + coalesce(cast(home_onboarding as int),0) + coalesce(cast(community_engagement_onboarded as int),0)) as reported_registrations
         from activities
         group by 1,2
-        order by  sum(coalesce(cast(centre_onboarding as int),0) + coalesce(cast(home_onboarding as int),0) + coalesce(cast(follow_up as int),0) + coalesce(cast(community_engagement_onboarded as int),0)) desc
+        order by  sum(coalesce(cast(centre_onboarding as int),0) + coalesce(cast(home_onboarding as int),0) + coalesce(cast(community_engagement_onboarded as int),0)) desc
     )
 select 
     calculating_beneficiaries_centres.*,
