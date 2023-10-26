b9b5b5eef6636afefa949d951e69b977d0ab6873
goalkeep@dosteducation.com <goalkeep@dosteducation.com>
2023-10-26 11:48:13 +0000

    add dost team member name , add percent over reported and add centre visited

--- models/marts/slg/fct_daily_registration_blockwise.sql
+++ models/marts/slg/fct_daily_registration_blockwise.sql
@@ -3,6 +3,7 @@ with
     geographies as (select * from {{ ref("int_geographies") }}),
     registrations as (select * from {{ ref("stg_registration") }}),
     activities as (select * from {{ ref("fct_activities") }}),
+    dost_team as (select * from {{ ref('stg_dost_team') }}),
 
     cross_join_dates as (select * from dates cross join geographies),
 
@@ -93,10 +94,46 @@ with
             end as registration_on_app
         from rollup_registrations_on_database
         left join calculate_registrations_on_app using (activity_level, activity_level_id, date)
+    ),
+    add_centre_visit as (
+        select 
+            cast(date_of_meeting as date) as date,
+            centre_name,
+            count(distinct centre_id) as centre_visited
+        from activities
+        where activity_type = 'Centre Visit Onboarding'
+        group by 1,2
+    ),
+    join_centre_visit_with_main_table as (
+        select
+            *
+        from cross_join_dates
+        left join add_centre_visit using (date,centre_name)
+    ),
+    rolling_up_centre_visited as (
+        select
+            join_registrations_on_app.*,
+            case
+                when activity_level = 'Centre' then sum(centre_visited) over (partition by join_registrations_on_app.centre_id, date)
+                when activity_level = 'Sector' then sum(centre_visited) over (partition by join_registrations_on_app.sector_id, date)
+                when activity_level = 'Block' then sum(centre_visited) over (partition by join_registrations_on_app.block_id, date)
+                when activity_level = 'District' then sum(centre_visited) over (partition by join_registrations_on_app.district_id, date)
+                when activity_level = 'State' then sum(centre_visited) over (partition by join_registrations_on_app.state_id, date)                
+            end as centre_visited
+        from join_registrations_on_app
+        left join join_centre_visit_with_main_table using (activity_level, activity_level_id, date)
+    ),
+    add_dost_info as (
+        select
+            rolling_up_centre_visited.*,
+            dost_member_name
+        from rolling_up_centre_visited
+        left join dost_team on rolling_up_centre_visited.sectors_assigned_to_id = dost_team.dost_team_id
     )
-
-select *
-from join_registrations_on_app
+select *, ((coalesce(registration_on_app,0) - coalesce(registrations_on_database,0)) / registrations_on_database) as percent_over_reported
+from add_dost_info
+where registration_on_app is not null
+-- where dost_member_name is not null
 -- where registrations_on_database is not null
 -- where sectors_assigned_to_id is not null
 -- where activity_level = 'Block'
